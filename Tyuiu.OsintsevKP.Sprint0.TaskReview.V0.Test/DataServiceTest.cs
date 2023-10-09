e0a89cea53ff26c3b291ea97d68f550fb05f960a
Tyuiu.OsintsevKP@outlook.com <osintcevkp@mail.ru>
2023-10-10 00:30:19 +0500

    Add project files.

---
+++ Tyuiu.OsintsevKP.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.OsintsevKP.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.OsintsevKP.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            Assert.AreEqual(20, DataService.Calc(1, 2, 1));
+        }
+    }
+}
---
+++ Tyuiu.OsintsevKP.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.OsintsevKP.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.OsintsevKP.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил: Осинцев К. П. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: SprintReview                                                      *");
+            Console.WriteLine("* Задание SprintReview #0                                                 *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Осинцев Константин Павлович | ИИПб-23-3                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на С#, которая суммирует три числа        *");
+            Console.WriteLine("* и умножает их на пять.                                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.Write("Введите первое число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(DataService.Calc(x,y,z));
+            Console.ReadKey();
+
+        }
+    }
+}
